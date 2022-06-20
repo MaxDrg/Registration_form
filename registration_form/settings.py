9bdf3c0c34596b69f51c409a87b2c0eea69fca5e
MaxDrg <maximadda2001@gmail.com>
2022-06-20 16:10:08 +0200

    Profession dropdown list

--- registration_form/settings.py
+++ registration_form/settings.py
@@ -134,6 +134,10 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = "/staticfiles/"
 
+STATICFILES_DIRS = [
+    BASE_DIR / "forum/templates/assets"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
