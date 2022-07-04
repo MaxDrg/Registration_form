5f5c6a9a8a928e3e1b4e2f42eb19c27fe7c34d5a
MaxDrg <maximadda2001@gmail.com>
2022-07-04 07:49:29 +0200

    Change url names

--- form/urls.py
+++ form/urls.py
@@ -3,7 +3,7 @@ from . import views
 # import hashlib
 
 urlpatterns = [
-    path('form', views.form, name='form'),
+    path('', views.form, name='form'),
     #path('thank', views.thank, name='thank')
     #path(hashlib.sha256('buckets'.encode('utf8')).hexdigest(), views.buckets, name='buckets')
 ]
--- registration_form/urls.py
+++ registration_form/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('registration/', include('form.urls')),
+    path('', include('form.urls')),
 ]
 
 if settings.DEBUG:
