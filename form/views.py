8b156c5d1b80485ce34dc5ea88fd9ec4332dcaa9
MaxDrg <maximadda2001@gmail.com>
2022-07-04 14:25:06 +0200

    Created post, files variables, created files checking function

--- form/views.py
+++ form/views.py
@@ -1,47 +1,49 @@
+from django.utils.datastructures import MultiValueDict
+from django.http.request import QueryDict
 from django.http import HttpRequest
 from django.shortcuts import render
 from . import models
 
 def form(request: HttpRequest):
     if request.method == "POST":
+        post: QueryDict = request.POST
+        files: MultiValueDict = request.FILES
+
         check_title = lambda title: title if not title == 'Choose a value' else ''
-        check_others = lambda current, other: request.POST[other] if current == 'other' else current
-        check_checkbox = lambda check, file: request.POST[file] if not check == 'none' else None
-        check_file = lambda check, data_name: request.FILES[data_name] if not check == 'none' and request.FILES[data_name] else None
-        check_image = lambda check, data_name: request.FILES[data_name] if check == '1' and request.FILES[data_name] else None
+        check_others = lambda current, other: post[other] if current == 'other' else current
+        check_checkbox = lambda check, file: post[file] if not check == 'none' else None
+        check_file = lambda check, data_name: files[data_name] if not check == 'none' and file_exist(files, data_name) else None
+        check_image = lambda check, data_name: files[data_name] if check == '1' and file_exist(files, data_name) else None
         check_access = lambda abstract, cv, check: 1 if abstract and cv and check == '1' else 0
 
-        print(f"file = {request.FILES['form4presentation-upload']}")
-        print(f"image = {request.FILES['form4portrait-upload']}")
+        academic_title = check_title(post['form4title[]'])
 
-        academic_title = check_title(request.POST['form4title[]'])
-
-        title = f"{request.POST['form4given-name']} {request.POST['form4family-name']}"
+        title = f"{post['form4given-name']} {post['form4family-name']}"
         if academic_title:
-            title = f"{academic_title} {request.POST['form4given-name']} {request.POST['form4family-name']}"
+            title = f"{academic_title} {post['form4given-name']} {post['form4family-name']}"
 
-        announce = request.POST.get('form4announce')
-        abstract = check_checkbox(request.POST['form4abstract-enter'], 'form4abstract')
-        cv = check_checkbox(request.POST['form4short-cv-enter'], 'form4short-cv')
+        announce = post.get('form4announce')
+        abstract = check_checkbox(post['form4abstract-enter'], 'form4abstract')
+        cv = check_checkbox(post['form4short-cv-enter'], 'form4short-cv')
         image = check_image(announce, 'form4portrait-upload')
 
         data = models.Content(
             academic_title      = academic_title,
-            given_name          = request.POST['form4given-name'],
-            family_name         = request.POST['form4family-name'],
-            gender              = request.POST['form4gender[]'],
-            email               = request.POST['form4email'],
-            telephone           = request.POST['form4telephone'],
-            academic_status     = check_others(request.POST['form4academic-status[]'], 'form4academic-status-other'),
-            country_origin      = check_others(request.POST['form4country-origin[]'], 'form4origin-other'),
-            current_location    = check_others(request.POST['form4current-location[]'], 'form4current-location-other'),
-            profession          = request.POST.getlist('form4profession[]'),
-            university          = request.POST['form4university'],
-            type_participation  = request.POST['form4participation-type'],
-            presentation_title  = request.POST['form4presentation-title'],
+            given_name          = post['form4given-name'],
+            family_name         = post['form4family-name'],
+            gender              = post['form4gender[]'],
+            email               = post['form4email'],
+            telephone           = post['form4telephone'],
+            academic_status     = check_others(post['form4academic-status[]'], 'form4academic-status-other'),
+            country_origin      = check_others(post['form4country-origin[]'], 'form4origin-other'),
+            current_location    = check_others(post['form4current-location[]'], 'form4current-location-other'),
+            profession          = post.getlist('form4profession[]'),
+            university          = post['form4university'],
+            type_participation  = post['form4participation-type'],
+            presentation_title  = post['form4presentation-title'],
             abstract            = abstract,
             short_cv            = cv,
-            presentation_upload = check_file(request.POST['form4presentation-upload-option'], 'form4presentation-upload'),
+            presentation_upload = check_file(post['form4presentation-upload-option'], 'form4presentation-upload'),
             portrait            = image
         )
         data.save()
@@ -69,9 +71,9 @@ def form(request: HttpRequest):
         )
         data.save()
 
-        tags = request.POST.getlist('form4profession[]')
+        tags = post.getlist('form4profession[]')
 
-        for tag in request.POST.get('form4other-profession').split('|'):
+        for tag in post.get('form4other-profession').split('|'):
             if not tag == '':
                 new_tag = models.ConfTags.objects.filter(title=tag).exists()
                 if not new_tag:
@@ -89,7 +91,7 @@ def form(request: HttpRequest):
                 new_tag.save()
                 tags.append(new_tag.id)
             else:
-                print('test')
+                print(bool(tag))
 
         for tag in tags:
             models.ConfContentitemTagMap(
@@ -106,3 +108,9 @@ def form(request: HttpRequest):
         {
             'professions': models.ConfTags.objects.all()
         })
+
+def file_exist(files: MultiValueDict, key: str):
+    for current_key in files.keys():
+        if current_key == key:
+            return True
+    return False
