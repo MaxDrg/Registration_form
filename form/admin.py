e2cc8edfe02849c9209f06507c776a7ff8473d71
MaxDrg <maximadda2001@gmail.com>
2022-07-15 15:31:15 +0200

    Debug datetimes fields

--- form/admin.py
+++ form/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from .models import ConfsepContent, Content
 
-admin.site.register(ConfsepContent)
+
+@admin.register(ConfsepContent)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'images')
+
 admin.site.register(Content)
