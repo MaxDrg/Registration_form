4e9fc2ad593f058da0f4ab42f16641536a06d13f
MaxDrg <maximadda2001@gmail.com>
2022-07-15 15:48:01 +0200

    Debug datetimes fields

--- form/admin.py
+++ form/admin.py
@@ -1,13 +1,22 @@
 from django.contrib import admin
+from django import forms
 from .models import ConfsepContent, Content
+
+class PersonAdminForm(forms.ModelForm):
+    class Meta:
+        model = ConfsepContent
+        fields = "__all__"
+
+    def clean_first_name(self):
+        if self.cleaned_data["title"] == "Spike":
+            raise forms.ValidationError("No Vampires")
+
+        return self.cleaned_data["first_name"]
 
 
 @admin.register(ConfsepContent)
 class BookInstanceAdmin(admin.ModelAdmin):
-    readonly_fields = ('checked_out_time',)
-
-    def my_custom_field(self, obj):
-        return 'Return Anything Here'
+    form = PersonAdminForm
     
 
 admin.site.register(Content)
