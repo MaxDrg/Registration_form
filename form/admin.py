3c978f4cc66b8d91fabe31679d3a2f86d57669df
MaxDrg <maximadda2001@gmail.com>
2022-07-15 15:40:38 +0200

    Debug datetimes fields

--- form/admin.py
+++ form/admin.py
@@ -4,6 +4,10 @@ from .models import ConfsepContent, Content
 
 @admin.register(ConfsepContent)
 class BookInstanceAdmin(admin.ModelAdmin):
-    readonly_fields = ('title', 'images')
+    readonly_fields = ('checked_out_time',)
+
+    def my_custom_field(self, obj):
+        return 'Return Anything Here'
+    
 
 admin.site.register(Content)
